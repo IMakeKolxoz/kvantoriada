dc49c820438767a197f421068fa6b44e30242697
IMakeKolxoz <74037210+IMakeKolxoz@users.noreply.github.com>
2021-08-06 12:16:46 +0900

    Update main.cpp

---
+++ code/c++/main.cpp
@@ -0,0 +1,40 @@
+// настройки манипулятор
+const int shoulder_1 = 210;  // в миллиметраx
+const int shoulder_2 = 135;  // в миллиметрах
+const int base = 155;  // расстояние от дна до сервы в миллиметрах
+//объявляем все переменные
+///координаты
+float x;  
+float y;
+float z;
+/*//стороны треугольника №1
+float side_1;  
+float side_2;
+float side_3;
+///стороны треугольника №2
+float shoulder_1;  
+float shoulder_2;
+//косинус и углы в радианах
+float cos_a;  
+float cos_b;
+float cos_c;
+float radian_a;  
+float radian_b;
+float radian_c;
+/*/
+//вводим координаты
+count << "Ввведите x: ";
+cin >> x;
+count << "Ввведите y: ";
+cin >> y;
+count << "Ввведите z: ";
+cin >> z;
+count << "х = " << x << endl;
+count << "y = " << y << endl;
+count << "z = " <<  << endl;
+
+
+void calculation of angles()
+{
+	
+}
