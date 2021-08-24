31a22a5cc5994f450f1d564a1336064d5c12bfff
IMakeKolxoz <74037210+IMakeKolxoz@users.noreply.github.com>
2021-08-24 12:48:07 +0900

    Delete 1.py

--- code/python/1.py
+++
@@ -1,5 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-response = requests.get("https://imakekolxoz.github.io/test/")
-soup = BeautifulSoup(response.content, 'html.parser')
-print(soup.body.string)
